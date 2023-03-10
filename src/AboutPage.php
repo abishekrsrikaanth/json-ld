7a307649e25f498bd05ef65caeb82427d59d6797
Abishek R Srikaanth <abishekrsrikaanth@users.noreply.github.com>
2023-03-10 22:23:05 +0530

    Adding AboutPage ContextType

---
+++ src/AboutPage.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AntonAm\JsonLD\ContextTypes;
+
+/**
+ * https://schema.org/WebSite
+ */
+class AboutPage extends CreativeWork
+{
+    /**
+     * Property structure
+     *
+     * @var array
+     */
+    protected $structure = [];
+}
