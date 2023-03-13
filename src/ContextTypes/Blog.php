4761d182ec040fa5573eb12a0611d08a2e317860
Abishek R Srikaanth <abishekrsrikaanth@users.noreply.github.com>
2023-03-13 14:42:00 +0000

    Adding Schema Type for Blog

---
+++ src/ContextTypes/Blog.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AntonAm\JsonLD\ContextTypes;
+
+/**
+ * https://schema.org/AboutPage
+ */
+class AboutPage extends CreativeWork
+{
+    /**
+     * Property structure
+     *
+     * @var array
+     */
+    protected $structure = [];
+}
